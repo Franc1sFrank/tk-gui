c3d29e3eef58c47c1424632094547bd89667fa41
Franc1sFrank <mr.fangzhou90@gmail.com>
2020-06-01 21:26:22 -0400

    Format text

--- tk-gui.py
+++ tk-gui.py
@@ -3,7 +3,7 @@ import tkinter as tk
 app = tk.Tk()
 app.title('App')
 
-lb = tk.Label(app, text='You are so beautiful!')
+lb = tk.Label(app, text='You are so beautiful!', width = 20, height = 10, fg = 'red')
 lb.pack()
 
 tk.mainloop()
