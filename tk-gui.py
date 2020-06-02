9d7d16569325aaa05976d3fd11a325ec01474563
Franc1sFrank <mr.fangzhou90@gmail.com>
2020-06-01 21:31:35 -0400

    Add event

--- tk-gui.py
+++ tk-gui.py
@@ -6,7 +6,10 @@ app.title('App')
 lb = tk.Label(app, text='You are so beautiful!', width = 20, height = 10, fg = 'red')
 lb.pack()
 
-bt = tk.Button(text = 'Exit', width = 10, bg = 'pink')
+def change_label():
+    lb.config(text = 'Thank you', fg = 'black')
+
+bt = tk.Button(text = 'Exit', width = 10, bg = 'pink', command = change_label)
 bt.pack()
 
 tk.mainloop()
