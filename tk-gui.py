2f68ec5af712d19e8fcb4aecb3236a663a66e3a7
Franc1sFrank <mr.fangzhou90@gmail.com>
2020-06-02 09:08:38 -0400

    Add text

--- tk-gui.py
+++ tk-gui.py
@@ -3,18 +3,21 @@ import tkinter as tk
 app = tk.Tk()
 app.title('App')
 
-lb = tk.Label(app, text='You are so beautiful!', width = 20, height = 10, fg = 'red')
+lb = tk.Label(app, text='You are so beautiful!', width=20, height=10, fg='red')
 lb.pack()
 
-def change_label():
-    lb.config(text = 'Thank you', fg = 'black')
 
-text = tk.Entry(app, width = 10)
+def change_label():
+    lb.config(text='Thank you', fg='black')
+
+
+text = tk.Entry(app, width=10)
 text.pack()
 
-bt = tk.Button(text = 'Exit', width = 10, bg = 'pink', command = change_label)
+txt = tk.Text()
+txt.pack()
+
+bt = tk.Button(text='Exit', width=10, bg='pink', command=change_label)
 bt.pack()
 
 tk.mainloop()
-
-
