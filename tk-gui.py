1daaeb78c07ffba42a1655256fabc345fc0a8e51
Franc1sFrank <mr.fangzhou90@gmail.com>
2020-06-02 09:06:19 -0400

    Remove textbox

--- tk-gui.py
+++ tk-gui.py
@@ -3,18 +3,15 @@ import tkinter as tk
 app = tk.Tk()
 app.title('App')
 
-lb = tk.Label(app, text='You are so beautiful!', width = 20, height = 10, fg = 'red')
+lb = tk.Label(app, text='You are so beautiful!', width=20, height=10, fg='red')
 lb.pack()
 
+
 def change_label():
-    lb.config(text = 'Thank you', fg = 'black')
+    lb.config(text='Thank you', fg='black')
 
-text = tk.Entry(app, width = 10)
-text.pack()
 
-bt = tk.Button(text = 'Exit', width = 10, bg = 'pink', command = change_label)
+bt = tk.Button(text='Exit', width=10, bg='pink', command=change_label)
 bt.pack()
 
 tk.mainloop()
-
-
