29992f53d7229cb394570954efcf272347d5bd9d
Franc1sFrank <mr.fangzhou90@gmail.com>
2020-06-01 21:27:49 -0400

    Add button

--- tk-gui.py
+++ tk-gui.py
@@ -6,6 +6,9 @@ app.title('App')
 lb = tk.Label(app, text='You are so beautiful!', width = 20, height = 10, fg = 'red')
 lb.pack()
 
+bt = tk.Button(text = 'Exit', width = 10, bg = 'pink')
+bt.pack()
+
 tk.mainloop()
 
 
